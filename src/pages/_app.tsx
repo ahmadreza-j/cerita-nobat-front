e3955329822c8de347c9dbcbefca028fdd677f8a
Ahmadreza <a.joolaee@gmail.com>
2023-08-24 18:20:12 +0330

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@/styles/fonts.css';
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
